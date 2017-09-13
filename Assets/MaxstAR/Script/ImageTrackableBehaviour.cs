6afd89d6a1487685eccdc225126b2a4584982fca
gskwon <gskwon@maxst.com>
2017-09-13 19:35:17 +0900

    이미지 트래커에 Terrain 붙혀 보자

---
+++ Assets/MaxstAR/Script/ImageTrackableBehaviour.cs
@@ -0,0 +1,68 @@
+﻿using UnityEngine;
+using System.IO;
+using JsonFx.Json;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Text;
+using UnityEngine.Rendering;
+
+namespace maxstAR
+{
+    public class ImageTrackableBehaviour : AbstractImageTrackableBehaviour
+    {
+		public override void OnTrackSuccess(string id, string name, Matrix4x4 poseMatrix)
+        {
+            Renderer[] rendererComponents = GetComponentsInChildren<Renderer>(true);
+            Collider[] colliderComponents = GetComponentsInChildren<Collider>(true);
+			Terrain[] terrainComponents = GetComponentsInChildren<Terrain>(true);
+
+            // Enable renderers
+            foreach (Renderer component in rendererComponents)
+            {
+                component.enabled = true;
+            }
+
+            // Enable colliders
+            foreach (Collider component in colliderComponents)
+            {
+                component.enabled = true;
+            }
+
+			// Enable terrain
+			foreach (Terrain component in terrainComponents)
+			{
+				component.enabled = true;
+			}
+
+			transform.position = MatrixUtils.PositionFromMatrix(poseMatrix);
+			transform.rotation = MatrixUtils.QuaternionFromMatrix(poseMatrix);
+			transform.localScale = MatrixUtils.ScaleFromMatrix(poseMatrix);
+        }
+
+        public override void OnTrackFail()
+        {
+            Renderer[] rendererComponents = GetComponentsInChildren<Renderer>(true);
+            Collider[] colliderComponents = GetComponentsInChildren<Collider>(true);
+			Terrain[] terrainComponents = GetComponentsInChildren<Terrain>(true);
+
+            // Disable renderer
+            foreach (Renderer component in rendererComponents)
+            {
+                component.enabled = false;
+            }
+
+            // Disable collider
+            foreach (Collider component in colliderComponents)
+            {
+                component.enabled = false;
+            }
+
+			// Disable terrain
+			foreach (Terrain component in terrainComponents)
+			{
+				component.enabled = false;
+			}
+        }
+    }
+}
